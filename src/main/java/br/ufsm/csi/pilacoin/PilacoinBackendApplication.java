e61add6fb2299548a9442bfd4c1876839427d8bb
Giovanni Sacchet <giovanni_xs@hotmail.com>
2023-12-01 15:56:18 -0300

    Initial commit

---
+++ src/main/java/br/ufsm/csi/pilacoin/PilacoinBackendApplication.java
@@ -0,0 +1,13 @@
+package br.ufsm.csi.pilacoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilacoinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilacoinBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufsm/csi/pilacoin/PilacoinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufsm.csi.pilacoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilacoinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
